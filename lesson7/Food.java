2185d1aa4c7b51651e3ce9bb13f6da3cbe29f6b4
YuliiaDenn <bnk4722@gmail.com>
2020-01-23 13:42:22 +0200

    HashMap+

--- lesson7/Food.java
+++ lesson7/Food.java
@@ -14,8 +14,39 @@ public class Food {
 		return kindOfFood;
 	}
 
-	public String getAmount() {
+	public String getkindOfFood() {
 		return nameFood;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kindOfFood == null) ? 0 : kindOfFood.hashCode());
+		result = prime * result + ((nameFood == null) ? 0 : nameFood.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Food other = (Food) obj;
+		if (kindOfFood == null) {
+			if (other.kindOfFood != null)
+				return false;
+		} else if (!kindOfFood.equals(other.kindOfFood))
+			return false;
+		if (nameFood == null) {
+			if (other.nameFood != null)
+				return false;
+		} else if (!nameFood.equals(other.nameFood))
+			return false;
+		return true;
 	}
 
 	@Override
--- lesson7/Shop.java
+++ lesson7/Shop.java
@@ -5,23 +5,24 @@ import java.util.Map;
 
 public class Shop {
 
-	public static void main(String[] args) {
+	Map<Food, Integer> shop = new HashMap<>();
 
-		Food banana = new Food("Fruits", "banana");
-		Food kiwi = new Food("Fruits", "kiwi");
-		Food potato = new Food("Vegetables", "potato");
-		Map<Food, Integer> food = new HashMap<>();
+	public void addProduct(Food food, int value) {
 
-		food.put(banana, 3);
-		food.put(kiwi, 7);
-		food.put(banana, 8);
-		food.put(potato, 12);
-
-		for (Map.Entry<Food, Integer> entry : food.entrySet()) {
-			System.out.println(entry.getKey() + " - " + entry.getValue() + " kilo");
+		if (food.hashCode() == shop.keySet().hashCode()) {
+			int otherValue = shop.get(food);
+			shop.put(food, otherValue + value);
+		} else {
+			shop.put(food, value);
 		}
-		
-		System.out.println("How many kilo of potatoes are there? - " +food.get(potato));
 
+	}
+
+	public int quantityByName(Food food) {
+		if (shop.containsKey(food)) {
+			return shop.get(food);
+		} else {
+			return 0;
+		}
 	}
 }
